5ee5262b1d442a1b7174496af5e50e60f6b03f13
brianLikesChips <brianjoo500@gmail.com>
2023-10-17 20:08:08 -0700

    t1

--- 1/tests/test_routes.py
+++ 1/tests/test_routes.py
@@ -1,7 +1,7 @@
 """
 This file contains the functional tests for the routes.
 These tests use GETs and POSTs to different URLs to check for the proper behavior.
-Resources:
+Resources:aaa
     https://flask.palletsprojects.com/en/1.1.x/testing/ 
     https://www.patricksoftwareblog.com/testing-a-flask-application-using-pytest/ 
 """
@@ -29,35 +29,39 @@ def test_client():
     # Flask provides a way to test your application by exposing the Werkzeug test Client
     # and handling the context locals for you.
     testing_client = flask_app.test_client()
- 
+
     # Establish an application context before running the tests.
     ctx = flask_app.app_context()
     ctx.push()
- 
-    yield  testing_client 
+
+    yield testing_client
     # this is where the testing happens!
- 
+
     ctx.pop()
+
 
 @pytest.fixture
 def new_user():
-    user = Student(username='sakire', email='sakire@wsu.edu',firstname='Sakire',lastname='Arslan Ay', address='Pullman, WA')
+    user = Student(username='sakire', email='sakire@wsu.edu',
+                   firstname='Sakire', lastname='Arslan Ay', address='Pullman, WA')
     user.set_password('1234')
     return user
 
+
 @pytest.fixture
-def init_database(request,test_client):
+def init_database(request, test_client):
     # Create the database and the database table
     db.create_all()
     # initialize the majors
     if Major.query.count() == 0:
-        majors = [{'name':'CptS','department':'School of EECS'},{'name':'SE','department':'Schoolof EECS'},{'name':'EE','department':'School of EECS'},
-                  {'name':'ME','department':'Mechanical Engineering'}, {'name':'MATH','department': 'Mathematics'}  ]
+        majors = [{'name': 'CptS', 'department': 'School of EECS'}, {'name': 'SE', 'department': 'Schoolof EECS'}, {'name': 'EE', 'department': 'School of EECS'},
+                  {'name': 'ME', 'department': 'Mechanical Engineering'}, {'name': 'MATH', 'department': 'Mathematics'}]
         for t in majors:
-            db.session.add(Major(name=t['name'],department=t['department']))
+            db.session.add(Major(name=t['name'], department=t['department']))
         db.session.commit()
-    #add a user    
-    user1 = Student(username='sakire', email='sakire@wsu.edu',firstname='Sakire',lastname='Arslan Ay', address='Pullman, WA')
+    # add a user
+    user1 = Student(username='sakire', email='sakire@wsu.edu',
+                    firstname='Sakire', lastname='Arslan Ay', address='Pullman, WA')
     user1.set_password('1234')
     # Insert user data
     db.session.add(user1)
@@ -68,7 +72,8 @@ def init_database(request,test_client):
 
     db.drop_all()
 
-def test_register_page(request,test_client):
+
+def test_register_page(request, test_client):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/register' page is requested (GET)
@@ -79,92 +84,100 @@ def test_register_page(request,test_client):
     assert response.status_code == 200
     assert b"Register" in response.data
 
-def test_register(request,test_client,init_database):
+
+def test_register(request, test_client, init_database):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/register' form is submitted (POST)
     THEN check that the response is valid and the database is updated correctly
     """
     # Create a test client using the Flask application configured for testing
-    response = test_client.post('/register', 
-                          data=dict(username='john', email='john@wsu.edu',password="bad-bad-password",password2="bad-bad-password", firstname='John',lastname='Yates', address='Pullman, WA'),
-                          follow_redirects = True)
+    response = test_client.post('/register',
+                                data=dict(username='john', email='john@wsu.edu', password="bad-bad-password",
+                                          password2="bad-bad-password", firstname='John', lastname='Yates', address='Pullman, WA'),
+                                follow_redirects=True)
     assert response.status_code == 200
 
-    s = db.session.query(Student).filter(Student.username=='john')
+    s = db.session.query(Student).filter(Student.username == 'john')
     assert s.first().lastname == 'Yates'
     assert s.count() == 1
     assert b"Please log in to access this page." in response.data
     assert b"Congratulations, you are now a registered user!" in response.data
-    assert b"Sign In" in response.data   
+    assert b"Sign In" in response.data
 
-def test_invalidlogin(request,test_client,init_database):
+
+def test_invalidlogin(request, test_client, init_database):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/login' form is submitted (POST) with wrong credentials
     THEN check that the response is valid and login is refused 
     """
-    response = test_client.post('/login', 
-                          data=dict(username='sakire', password='12345',remember_me=False),
-                          follow_redirects = True)
+    response = test_client.post('/login',
+                                data=dict(username='sakire',
+                                          password='12345', remember_me=False),
+                                follow_redirects=True)
     assert response.status_code == 200
     assert b"Invalid username or password" in response.data
 
-def test_login_logout(request,test_client,init_database):
+
+def test_login_logout(request, test_client, init_database):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/login' form is submitted (POST) with correct credentials
     THEN check that the response is valid and login is succesfull 
     """
-    response = test_client.post('/login', 
-                          data=dict(username='sakire', password='1234',remember_me=False),
-                          follow_redirects = True)
+    response = test_client.post('/login',
+                                data=dict(username='sakire',
+                                          password='1234', remember_me=False),
+                                follow_redirects=True)
     assert response.status_code == 200
     assert b"Hi, Sakire Arslan Ay!" in response.data
 
-    response = test_client.get('/logout',                       
-                          follow_redirects = True)
+    response = test_client.get('/logout',
+                               follow_redirects=True)
     assert response.status_code == 200
     assert b"Sign In" in response.data
-   
 
-def test_createclass(request,test_client,init_database):
+
+def test_createclass(request, test_client, init_database):
     """
     GIVEN a Flask application configured for testing , after user logs in,
     WHEN the '/createclass' page is requested (GET)  AND /createclass' form is submitted (POST)
     THEN check that response is valid and the class is successfully created in the database
     """
-    #first login
-    response = test_client.post('/login', 
-                          data=dict(username='sakire', password='1234',remember_me=False),
-                          follow_redirects = True)
+    # first login
+    response = test_client.post('/login',
+                                data=dict(username='sakire',
+                                          password='1234', remember_me=False),
+                                follow_redirects=True)
     assert response.status_code == 200
     assert b"Hi, Sakire Arslan Ay!" in response.data
-    
-    #test the "create class" form 
+
+    # test the "create class" form
     response = test_client.get('/createclass')
     assert response.status_code == 200
     assert b"Create a new class" in response.data
-    
-    #test posting a class
-    response = test_client.post('/createclass', 
-                          data=dict(coursenum = '355', title = 'Programming Languages', major = 'CptS'),
-                          follow_redirects = True)
+
+    # test posting a class
+    response = test_client.post('/createclass',
+                                data=dict(
+                                    coursenum='355', title='Programming Languages', major='CptS'),
+                                follow_redirects=True)
     assert response.status_code == 200
     assert b"CptS 355" in response.data
-    assert b"Programming Languages" in response.data 
-    c = db.session.query(Class).filter(Class.coursenum =='355')
+    assert b"Programming Languages" in response.data
+    c = db.session.query(Class).filter(Class.coursenum == '355')
     assert c.first().title == 'Programming Languages'
     assert c.count() == 1
 
-    #finally logout
-    response = test_client.get('/logout',                       
-                          follow_redirects = True)
+    # finally logout
+    response = test_client.get('/logout',
+                               follow_redirects=True)
     assert response.status_code == 200
     assert b"Sign In" in response.data
 
 
-def test_enroll(request,test_client,init_database):
+def test_enroll(request, test_client, init_database):
     """
     GIVEN a Flask application configured for testing , after user logs in, and after a class is created
     WHEN the '/enroll' form is submitted (POST)
@@ -179,7 +192,7 @@ def test_enroll(request,test_client,init_database):
 #     WHEN the '/' page is requested (GET)
 #     THEN check that the response is valid
 #     """
-#     response = test_client.post('/login', 
+#     response = test_client.post('/login',
 #                           data=dict(username='sakire', password='1234',remember_me=False),
 #                           follow_redirects = True)
 #     assert response.status_code == 200
