309ca9fe8465d691fe5521e5c142bce69615f9d8
joseph.steeb <joseph.steeb@wsu.edu>
2023-10-21 17:21:58 -0700

    added visualization, removed redundant folders

---
+++ app/Controller/auth_forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField , TextAreaField, PasswordField, BooleanField, SelectField
+from wtforms_sqlalchemy.fields import QuerySelectField
+from wtforms.validators import  ValidationError, Length, DataRequired, Email, EqualTo
+from app.Model.models import Affiliate
+from flask_login import current_user
+
+class affiliateRegister(FlaskForm):
+    firstname=StringField('First Name:',validators=[DataRequired()])
+    lastname=StringField('Last Name:',validators=[DataRequired()])
+    wsuCampus=SelectField('Campus:', choices=[('Please Select an Option Below'), ('WSU Pullman'), ('WSU Spokane'), ('WSU Tri-Cities'), ('WSU Vancouver'), ('WSU Everett'), ("WSU Global Campus")], validators=[DataRequired()])
+    department=SelectField('Department:', choices=[('Please Select an Option Below'), ('Anthropology'), ('Art'), ('Chemistry'), ('Criminal Justice and Criminology'), ('Digital Technology and Culture'), ('English'), ('History'), ('Mathematics and Statistics'), ('Physics and Astronomy'), ('Psychology'), ('Sociology')], validators=[DataRequired()])
+    url=StringField('URL:',validators=[DataRequired()])
+    email=StringField('Email:',validators=[DataRequired(),Email()])
+    password=PasswordField('Password:',validators=[DataRequired()])
+    password2=PasswordField('Repeat password:',validators=[DataRequired(),EqualTo('password')])
+    submit=SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email=StringField('Email:',validators=[DataRequired()])
+    password=PasswordField('Password:',validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Sign in')
+
+class AddKeywords(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class AdminEditProfile(FlaskForm):
+    firstname=StringField('First Name:',validators=[DataRequired()])
+    lastname=StringField('Last Name:',validators=[DataRequired()])
+    campus = StringField('Campus', validators=[DataRequired()])
+    department=StringField('Department:', validators=[DataRequired()])
+    areaofinterest =StringField('Area of Interest:')
+    URL = StringField('URL:')
+    is_admin = BooleanField('is_admin')
+    submit=SubmitField('Submit')
+
---
+++ app/__init__.py
@@ -0,0 +1,43 @@
+from ensurepip import bootstrap
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+from config import Config
+# extensions
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+login = LoginManager()
+login.login_view = 'auth.login'
+moment = Moment()
+
+
+def create_app(config_class=Config):
+
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    app.static_folder = config_class.STATIC_FOLDER
+    app.template_folder = config_class.TEMPLATE_FOLDER
+#    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:teamFullStack@localhost/our_users1'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:CEREO2023@localhost:3306/our_users1'
+    # instad of localhost i need ipss
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@remote_server_ip_or_hostname:port/database_name'
+
+    db.init_app(app)
+    login.init_app(app)
+    moment.init_app(app)
+    bootstrap.init_app(app)
+
+    # blue print reg
+    from app.Controller.errors import errors_blueprint as errors
+    app.register_blueprint(errors)
+
+    from app.Controller.auth_routes import auth_blueprint as auth
+    app.register_blueprint(auth)
+
+    from app.Controller.routes import routes_blueprint as routes
+    app.register_blueprint(routes)
+
+    return app
