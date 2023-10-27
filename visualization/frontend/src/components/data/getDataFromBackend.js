162513263871b83e5c6f1c9764b856b9d919a55d
joseph.steeb <joseph.steeb@wsu.edu>
2023-10-27 00:39:34 -0700

    moved vis team backend back to localhost because of weird error

--- visualization/frontend/src/components/data/getDataFromBackend.js
+++ visualization/frontend/src/components/data/getDataFromBackend.js
@@ -2,7 +2,9 @@
 // Description: This file contains functions that make requests to the backend to get data.
 
 // Define the base URL of the backend server
-const backendBaseUrl = "http://172.232.172.160:3004"; // Replace with your actual backend URL
+//const backendBaseUrl = "http://172.232.172.160:3004"; // Replace with your actual backend URL
+const backendBaseUrl = "http://localhost:3004";
+
 
 // Function to make a request to the backend to get all the entries
 export const filterEntries = (valueToFilterBy) => {
